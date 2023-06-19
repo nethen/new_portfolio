be5efff7d2c0f5d59b929a364c13832ce34de77a
amemichelle <54964331+amemichelle@users.noreply.github.com>
2023-06-19 01:00:23 -0700

    first pass at desktop styling

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,30 @@
+import "./Footer.scss";
+import singlesparkle from "../../assets/singlesparkle.svg";
+
+function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <div className="footer__left">
+          <p className="footer__copyright">© Michelle Swolfs 2023</p>
+        </div>
+
+        <div className="footer__right">
+          <a href="" className="footer__link">
+            Email ↗
+          </a>
+          <img className="footer__sparkle" src={singlesparkle} />
+          <a href="" className="footer__link">
+            Linkedin ↗
+          </a>
+          <img className="footer__sparkle" src={singlesparkle} />
+          <a href="" className="footer__link">
+            Github ↗
+          </a>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,12 +1,30 @@
 import "./home.scss";
 import Navbar from "../../components/Navbar/navbar";
 import TopParagrah from "../../components/TopParagraph/TopParagraph";
+import ProjectBlock from "../../components/ProjectBlock/projectblock";
+import Footer from "../../components/Footer/Footer";
+
+import guustologo from "../../assets/guusto.svg";
+import boxpalslogo from "../../assets/boxpals.svg";
 
 function Home() {
   return (
     <>
       <Navbar></Navbar>
       <TopParagrah></TopParagrah>
+      <ProjectBlock
+        img={guustologo}
+        title="Reimagining what employee recognition looks like"
+        desc="Design @ Guusto"
+        bgcolor="#22315c"
+      ></ProjectBlock>
+      <ProjectBlock
+        img={boxpalslogo}
+        title="Helping individuals save money through group order management"
+        desc="Development @ Brainstaiton"
+        bgcolor="#004921"
+      ></ProjectBlock>
+      <Footer></Footer>
     </>
   );
 }
